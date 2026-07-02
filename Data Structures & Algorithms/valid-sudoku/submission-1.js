fa357c61a9b2928df56cadd09fead78cda56c2aa
Divya Prakash <divyaprakash002@gmail.com>
2026-07-02 21:47:28 +0530

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {character[][]} board
+     * @return {boolean}
+     */
+    isValidSudoku(board) {
+        const rowHash = {};
+        const columnHash = {};
+        const squareHash = {};
+
+        for (let i = 0; i < 9; i++) {
+            for (let j = 0; j < 9; j++) {
+                if (board[i][j] == ".") continue;
+                let squareKey = `${Math.floor(i / 3)}${Math.floor(j / 3)}`;
+                if (
+                    (rowHash[i] && rowHash[i][board[i][j]]) ||
+                    (columnHash[j] && columnHash[j][board[i][j]]) ||
+                    (squareHash[squareKey] && squareHash[squareKey][board[i][j]])
+                )
+                    return false;
+                    if(!rowHash[i])rowHash[i]={};
+                    if(!columnHash[j])columnHash[j]={};
+                    if(!squareHash[squareKey])squareHash[squareKey]={};
+                    
+                    rowHash[i][board[i][j]]=true;
+                    columnHash[j][board[i][j]]=true;
+                    squareHash[squareKey][board[i][j]]=true;
+            }
+        }
+        return true;
+    }
+}

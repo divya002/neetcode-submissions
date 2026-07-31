c6b3a257c8dd222da6b8420b679eff397320ae59
Divya Prakash <divyaprakash002@gmail.com>
2026-07-31 21:26:38 +0530

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.js
@@ -0,0 +1,39 @@
+class MinStack {
+    constructor() {
+        this.stack=[];
+        this.minStack=[];
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        this.stack.push(val);
+        if(this.minStack.length==0 || this.minStack[this.minStack.length-1]>=val)
+        this.minStack.push(val);
+    }
+         
+    /**
+     * @return {void}
+     */
+    pop() {
+        let ele=this.stack.pop();
+        if(this.minStack[this.minStack.length-1]==ele)
+        this.minStack.pop()
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.stack[this.stack.length-1];
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        return this.minStack[this.minStack.length-1];
+    }
+}

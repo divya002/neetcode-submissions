4d3f22036b6651dd77e325ee555d09275cfa483e
Divya Prakash <divyaprakash002@gmail.com>
2026-08-14 00:10:11 +0530

    Add: decode-string - submission-5

---
+++ Data Structures & Algorithms/decode-string/submission-5.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {string}
+     */
+    decodeString(s) {
+        let stack = [];
+        for (let char of s) {
+            if (char == "]") {
+                let r = "";
+                while (stack[stack.length - 1] !== "[") {
+                    let pop = stack.pop();
+                    r = pop + r;
+                }
+                stack.pop();
+                let n = stack.pop();
+                while (!isNaN(stack[stack.length - 1])) {
+                    n = `${stack.pop()}${n}`;
+                }
+                while (n > 0) {
+                    stack.push(r);
+                    n--;
+                }
+            } else {
+                stack.push(char);
+            }
+        }
+        return stack.join("");
+    }
+}
